6f907087e27de91a368587390973adfc7be3f8eb
CarleeSinton <carleen.sinton@gmail.com>
2019-10-24 10:04:50 -0700

    Add bus nuget packages & add rabbitMQ to startups

---
+++ CartApi/Messaging/OrderCompletedEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CartApi.Messaging
+{
+    public class OrderCompletedEvent
+    {
+    }
+}
