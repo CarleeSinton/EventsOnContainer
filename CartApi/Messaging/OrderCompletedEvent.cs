ff32db1df7e70fa9ff7c89ab76ad4614cbc94d70
CarleeSinton <carleen.sinton@gmail.com>
2019-10-24 10:32:42 -0700

    OrderCompletedEvent & OrderCompletedEventConsumer

--- CartApi/Messaging/OrderCompletedEvent.cs
+++ CartApi/Messaging/OrderCompletedEvent.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CartApi.Messaging
+namespace Common.Messaging
 {
     public class OrderCompletedEvent
     {
+        public string BuyerId { get; set; }
+        public OrderCompletedEvent(string buyerId)
+        {
+            BuyerId = buyerId;
+        }
     }
 }
